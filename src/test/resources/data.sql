5d66171512bb03aa9eb43b64d37bddda87dd2169
Vally Bhamidipati <vallyprasuna@gmail.com>
2025-10-24 02:06:43 +0100

    Remove mocks for employee service impl

---
+++ src/test/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO EMPLOYEE
+(emp_name, emp_address, emp_contact_number, emp_salary, empdob, emp_email)
+VALUES
+    ('Ram K', 'India', 9852364712, 185699.68, '2004-05-23', 'ramk@gmail.com');
+INSERT INTO EMPLOYEE
+(emp_name, emp_address, emp_contact_number, emp_salary, empdob, emp_email)
+VALUES
+    ('John F', 'US', 9852364713, 85699.68, '2001-05-24', 'jfk@gmail.com');
+INSERT INTO EMPLOYEE
+(emp_name, emp_address, emp_contact_number, emp_salary, empdob, emp_email)
+VALUES
+    ('Rebecca V', 'UK', 9852364714, 285699.68, '2001-06-23', 'rebv@gmail.com');
